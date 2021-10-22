49f65fa8cb2db529f7921d6e634b2b81d227b02e
groiq <35578027+groiq@users.noreply.github.com>
2021-10-22 15:48:04 +0200

    set up tests for QueueImpl

--- src/test/java/impl/QueueImplTest.java
+++ src/test/java/impl/QueueImplTest.java
@@ -1,42 +1,58 @@
 package impl;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import skeleton.Person;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueImplTest {
 
-    @BeforeEach
-    void setUp() {
+    private static Person boilerplatePerson1 = new Employee("x", "x", "x", LocalDate.now(), "x", "x");
+
+    @Test
+    void testSizeOfEmptyQueue() {
+        // Given
+        QueueImpl given = new QueueImpl();
+
+        // When
+        int actual = given.size();
+
+        // Then
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 
     @Test
-    void add() {
-        QueueImpl myQueue = new QueueImpl();
+    void testAddingIncreasesSize() {
+        // Given
+        QueueImpl given = new QueueImpl();
 
-        String output = myQueue.toString();
+        // When
+        given.add(boilerplatePerson1);
 
-        assertEquals("Hello world", output);
-    }
-
-    @Test
-    void retrieve() {
-    }
-
-    @Test
-    void size() {
-    }
-
-    @Test
-    void clear() {
-    }
-
-    @Test
-    void iterator() {
+        // Then
+//        int actual =
     }
 
     @Test
     void testToString() {
+    }
+
+    @Test
+    void testRetrieve() {
+    }
+
+    @Test
+    void testClear() {
+    }
+
+    @Test
+    void testIterator() {
+    }
+
+    @Test
+    void testToString1() {
     }
 }
